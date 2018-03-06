89c25015fbecc5e4a5c2d1099bf5d19c1a38da15
Nick Taylor <nicholasjt22@gmail.com>
2018-03-06 00:23:06 -0800

    heroku deploy fix

--- server.js
+++ server.js
@@ -17,14 +17,16 @@ var app = express();
 app.use(logger('dev'));
 
 
-app.use('/', express.static(path.join(__dirname, '/build')));
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
-
 app.use(require('./config/auth'));
-
 app.use('/api/users', require('./routes/api/users'));
 
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 var port = process.env.PORT || 3001;
 
